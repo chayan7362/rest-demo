ed0f8d9caa82df4f1568f880a00fb536876e68bb
chayan7362 <chayanmaiti2023@gmail.com>
2024-12-01 14:57:26 +0530

    initialize global exception

--- src/main/java/com/springboot/restcontroller/rest/StudentRestController.java
+++ src/main/java/com/springboot/restcontroller/rest/StudentRestController.java
@@ -43,31 +43,4 @@ public class StudentRestController {
         return theStudents.get(studentId);
     }
 
-    // add an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMassage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // add another exception handler ... to catch any exception (Catch all)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMassage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
